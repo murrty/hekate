5397a0db23f86743d4ef67b38372c19049e73179
matty <10895809+murrty@users.noreply.github.com>
2020-05-09 23:24:14 -0500

    Rename "patches.ini" to "tinfoiledww.ini"
    lol

--- bootloader/hos/pkg2.c
+++ bootloader/hos/pkg2.c
@@ -661,7 +661,7 @@ static void parse_external_kip_patches()
 
 	u32 curr_kip_idx = 0;
 	char path[64];
-	strcpy(path, "bootloader/patches.ini");
+	strcpy(path, "bootloader/tinfoiledww.ini");
 
 	LIST_INIT(ini_kip_sections);
 	if (ini_patch_parse(&ini_kip_sections, path))
